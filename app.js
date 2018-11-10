0694a3acbe5ab9a546b63b9e9454c117fef46e7a
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-11-10 17:24:33 -0500

    Setting up Postgress db and Sequalize

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const express = require('express');
 const bodyparser = require('body-parser');  
 const html = require('html-template-tag');
 const app = express();
+const { main } = require('./views');
+const { db } = require('./models/');
 
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + './stylesheets'));
 app.use(bodyparser.urlencoded({extended: true}));
 
+app.get('/', (req, res, next) =>{
+    res.send(main(''));
+});
+
+
+
+
+db.authenticate().then(()=>{
+    console.log('Database Connected!');
+})
+
+app.listen(3000, () =>{
+    console.log(`App listening in 3000`);
+});
