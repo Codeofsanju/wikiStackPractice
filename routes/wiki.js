d2b9ce247fad5690bf047895bc0488ff73c6caab
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-11-11 23:00:45 -0500

    Post pages loading from database

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,5 +1,5 @@
 const Router = require('express').Router();
-const {addPage, main} = require('../views');
+const {addPage, main, wikiPage} = require('../views');
 const models = require('../models');
 
 Router.get('/', async(req, res, next)=>{
@@ -18,12 +18,25 @@ Router.post('/', async(req, res, next)=>{
     
     try{
         await page.save();
-        res.redirect('/');
+        res.redirect(`/wiki/${page.slug}`);
     }catch(err) {next(err);}
 });
 
 Router.get('/add', (req, res, next)=>{
     res.send(addPage());
+});
+
+Router.get('/:slug', async(req, res, next) =>{
+    // console.log(req.params.slug);
+    // res.send(`dynamic page for route at ${req.params.slug}`);
+    try{
+        const page = await models.Page.findOne({
+            where: {
+                slug: req.params.slug,
+            }
+        });
+        res.send(wikiPage(page));
+    }catch(error){next(error);}
 });
 
 module.exports = Router;
